6d13705584a6709a2a367dbdd1de2410bd559cee
Nick Nesler <nicknesler@gmail.com>
2015-03-13 20:15:17 -0400

    initializing enum for continents

--- Risk2210/src/com/nesler/risk/risk2210/ContinentConstants.java
+++ Risk2210/src/com/nesler/risk/risk2210/ContinentConstants.java
@@ -1,17 +1,29 @@
 package com.nesler.risk.risk2210;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
+
+import com.nesler.risk.risk2210.TerritoryConstants.Territories;
+
 public class ContinentConstants {
 	
+	//not sure if this should have an index number as well. . . why not do string keys? Benefits/drawbacks?
 	public enum Continents {
-		AFRICA, ASIA, AUSTRALIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA
+		AFRICA("Africa", 4), 
+		ASIA("Asia", 5), 
+		AUSTRALIA("Australia", 3), 
+		EUROPE("Europe", 4), 
+		NORTH_AMERICA("North America", 4), 
+		SOUTH_AMERICA("South America", 3);
+		
+		private String displayName;
+		private int bonus;
+		
+		
+		Continents(String displayName, int bonus){
+		this.displayName = 	displayName;
+		this.bonus = bonus;
+		}
 	}
-
-	/*
-	 * The following enums declare which territories exist in which continents.
-	 */
-	public enum SOUTH_AMERICA {
-		AMAZON_DESERT, ANDEAN_NATIONS, ARGENTINA, NUEVO_TIMOTO 
-	}
-	
-	String SOUTH_AMERICA = "South America";
 }
